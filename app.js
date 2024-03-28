b5e9612698204cc89da7b547d32b47ed6d6cdb75
Corben McClintock <s544086@nwmissouri.edu>
2024-03-27 20:59:16 -0500

    Completed part 5: Added in the files for my class

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var parentRouter = require('./routes/parent');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/parent', parentRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
